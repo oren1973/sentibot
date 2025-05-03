26b76bc8ee4009ba56e31cf2a83ff7b43f643baa
oren1973 <131968506+oren1973@users.noreply.github.com>
2025-05-03 16:45:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-from scanner import scan_market_and_generate_report
 
-# דיאגנוסטיקה של משתני סביבה
+# בדיקת משתני סביבה
 sender_email = os.getenv("SENDER_EMAIL")
 app_password = os.getenv("APP_PASSWORD")
 receiver_email = os.getenv("RECEIVER_EMAIL")
@@ -10,5 +9,4 @@ print("✅ DEBUG | SENDER_EMAIL:", sender_email)
 print("✅ DEBUG | APP_PASSWORD is None:", app_password is None)
 print("✅ DEBUG | RECEIVER_EMAIL:", receiver_email)
 
-# סיום מוקדם – רק הדפסת משתנים
 exit(0)
