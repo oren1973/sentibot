6c53449f22ef7ade7662f9007e0dd6bcc4397a49
oren1973 <131968506+oren1973@users.noreply.github.com>
2025-05-03 16:40:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import os
+from scanner import scan_market_and_generate_report
 import smtplib
 from email.mime.text import MIMEText
-from scanner import scan_news
+from datetime import datetime
+import os
 
 def send_email(subject, body):
     sender_email = os.getenv("SENDER_EMAIL")
     app_password = os.getenv("APP_PASSWORD")
     receiver_email = os.getenv("RECEIVER_EMAIL")
+
+    # הדפסת דיאגנוסטיקה
+    print("DEBUG | SENDER_EMAIL:", sender_email)
+    print("DEBUG | APP_PASSWORD is None:", app_password is None)
+    print("DEBUG | RECEIVER_EMAIL:", receiver_email)
 
     msg = MIMEText(body)
     msg["Subject"] = subject
@@ -18,7 +24,6 @@ def send_email(subject, body):
         server.sendmail(sender_email, receiver_email, msg.as_string())
 
 if __name__ == "__main__":
-    report = scan_news()
-    if report:
-        body = f"""Sentibot - דוח רגשי\n\n{report}"""
-        send_email("Sentibot | דוח אוטומטי", body)
+    report = scan_market_and_generate_report()
+    if report:  # שלח רק אם יש תוכן
+        send_email("Sentibot | דוח אוטומטי", report)
