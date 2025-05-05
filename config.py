0a72e25434e9378011772bcbc9c5c3f9df288a03
oren1973 <131968506+oren1973@users.noreply.github.com>
2025-05-05 22:58:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # 📄 config.py
 WHITELISTED_SYMBOLS = ["AAPL", "GOOGL", "MSFT", "AMZN", "NVDA", "META", "TSLA"]
 TRADE_THRESHOLD = 0.4  # רמת בטחון מינימלית להמלצה
+SYMBOLS = ["AAPL", "TSLA", "NVDA", "MSFT", "META"]
