3c6acc85058bf04f571fb0fbdc7f5d1d6b74b5d1
oren1973 <131968506+oren1973@users.noreply.github.com>
2025-05-31 17:41:11 +0300

    Create symbol_names.py

---
+++ symbol_names.py
@@ -0,0 +1,21 @@
+SYMBOL_TO_NAME = {
+    "TSLA": "Tesla",
+    "NVDA": "Nvidia",
+    "GME": "GameStop",
+    "AMC": "AMC",
+    "PLTR": "Palantir",
+    "COIN": "Coinbase",
+    "MSTR": "MicroStrategy",
+    "BYND": "Beyond Meat",
+    "RIVN": "Rivian",
+    "AFRM": "Affirm",
+    "SOFI": "SoFi",
+    "BB": "BlackBerry",
+    "BBBYQ": "Bed Bath & Beyond",
+    "NIO": "NIO",
+    "LCID": "Lucid",
+    "NKLA": "Nikola",
+    "AAPL": "Apple",
+    "META": "Meta",
+    "SNAP": "Snap"
+}
